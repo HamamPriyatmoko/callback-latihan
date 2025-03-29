599143d9fb338d29365c6fdaef4bf151e6ac09f3
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2025-03-29 13:30:22 +0700

    membuat modal interactive dengan apinya

--- script.js
+++ script.js
@@ -11,13 +11,42 @@ $.ajax({
             <div class="card-body">
               <h5 class="card-title">${movie.Title}</h5>
               <h6 class="card-subtitle mb-2 text-muted">${movie.Year}</h6>
-              <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#movieDetailModal">Show Details</a>
+              <a href="#" class="btn btn-primary modal-detail-button" data-toggle="modal" 
+              data-target="#movieDetailModal" data-imdbid="${movie.imdbID}">Show Details</a>
             </div>
           </div>
         </div>
             `;
     });
     $('.movie-container').html(cards);
+    $('.modal-detail-button').on('click', function () {
+      // console.log($(this).data('imdbid'));
+      $.ajax({
+        url: `http://www.omdbapi.com/?apikey=f281c8b5&i=` + $(this).data('imdbid'),
+        success: function (data) {
+          const movieDetail = `<div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-3">
+                        <img src="${data.Poster}" alt="" class="img-fluid"/>
+                    </div>
+                    <div class="col-md">
+                        <ul class="list-group">
+                            <li class="list-group-item"><h4>${data.Title} (2016)</h4></li>
+                            <li class="list-group-item"><strong>Director : ${data.Director}</strong></li>
+                            <li class="list-group-item"><strong>Actors :</strong> ${data.Actors}</li>
+                            <li class="list-group-item"><Strong>Genre :</Strong> ${data.Genre}</li>
+                            <li class="list-group-item"><strong>Plot :</strong> ${data.Plot}</li>
+                          </ul>
+                    </div>
+                </div>
+            </div>`;
+          $('.modal-body').html(movieDetail);
+        },
+        error: function (error) {
+          console.log('Error:', error.responseText);
+        },
+      });
+    });
   },
   error: function (error) {
     console.log('Error:', error.responseText);
